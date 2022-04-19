70bc8d22036d159b02a026b21508ae9c85cac6a4
LesyaNechkasova <58375442+Frr-fox@users.noreply.github.com>
2022-04-19 21:52:03 +0300

    Add test with creation question without authorization

--- src/test/java/lab3/usecase/QuestionCreationTest.kt
+++ src/test/java/lab3/usecase/QuestionCreationTest.kt
@@ -25,4 +25,20 @@ class QuestionCreationTest : TestWithDrivers() {
             }
         }
     }
+
+    @Test
+    fun tryCreateQuestionWithoutAuthorization() {
+        drivers?.parallelStream()?.forEach { driver ->
+            driver.get(Utils.BASE_URL)
+            driver.manage().window().size = Dimension(1100, 674)
+            with(driver) {
+                xpath("/html/body/div[1]/div[1]/div/div/div[1]/div/div[4]/div[1]/div/button/button").click()
+                xpath("/html/body/div[1]/div[1]/div/div/div[1]/div/div[4]/div[1]/div/div/div/span[1]/button").click()
+                xpath("/html/body/div[1]/div[1]/div/div/div[4]/div/div[2]/div/div[1]/button[2]").click()
+                val alert = xpath("/html/body/div[1]/div[1]/div/div/div[4]/div/div[2]/div")
+                val elementLoginWithEmail = xpath("/html/body/div[1]/div[1]/div/div/div[4]/div/div[2]/div/div[2]/div[2]/div/button[4]/span")
+                Assertions.assertEquals(elementLoginWithEmail.text, "Log in with email")
+            }
+        }
+    }
 }
